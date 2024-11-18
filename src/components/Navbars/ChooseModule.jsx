ce7071f1b7bd0f897a10d7aa0bd3ab4c9aea63e8
HW0470488 <helena.werner@campus.tu-berlin.de>
2024-11-18 17:05:16 +0100

    Add new Navigation component ChooseModule

---
+++ src/components/Navbars/ChooseModule.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Container,Dropdown } from "react-bootstrap";
+
+function ChooseModule() {
+    return (
+        <Container fluid>
+        <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
+          <Dropdown></Dropdown>
+        </div>
+      </Container>
+
+    );
+}
+
+export default ChooseModule;
