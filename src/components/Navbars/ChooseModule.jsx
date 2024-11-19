add4fe24da878c121fb18b2ee5797d7f693ddb26
HW0470488 <helena.werner@campus.tu-berlin.de>
2024-11-19 22:24:24 +0100

    implement dropdown menu
    problem: module titles are still not shown, probably the datas in the dropdown items are not called correctly

--- src/components/Navbars/ChooseModule.jsx
+++ src/components/Navbars/ChooseModule.jsx
@@ -1,15 +1,46 @@
-import React from "react";
-import {Container,Dropdown } from "react-bootstrap";
+import React, { useState, useEffect } from 'react';
+import { Container, Dropdown } from "react-bootstrap";
+import axios from "axios";
 
 function ChooseModule() {
-    return (
-        <Container fluid>
-        <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
-          <Dropdown></Dropdown>
-        </div>
-      </Container>
+  // Zustand für die Module
+  const [modules, setModules] = useState([]);
 
-    );
+  // URL des Backends
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"; 
+
+  // Daten beim ersten Render abrufen
+  useEffect(() => {
+    const fetchModules = async () => {
+      try {
+        const response = await axios.get(`${backendUrl}/modules/all?full=false`);
+        setModules(response.data); // Daten in den Zustand speichern
+      } catch (error) {
+        console.error("Fehler beim Abrufen der Module:", error);
+      }
+    };
+
+    fetchModules();
+  }, []); // Leer = nur einmal beim Mounten ausführen
+
+  return (
+    <Container fluid>
+        <Dropdown>
+          <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+            Choose Module
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Header>Choose Module</Dropdown.Header>
+            {/* Dropdown-Elemente */}
+            {modules.map((module, index) => (
+              <Dropdown.Item eventKey={index}>
+                {module.titelde} / {module.titelen}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+    </Container>
+  );
 }
 
 export default ChooseModule;
