0958c7f3991c9102f603cc92c2069ffb0ef31e54
k0nst1x <konsti.berlin@web.de>
2024-11-20 13:18:37 +0100

    Load module info for singular module onclick

--- src/components/Navbars/ChooseModule.jsx
+++ src/components/Navbars/ChooseModule.jsx
@@ -5,26 +5,35 @@ import axios from "axios";
 function ChooseModule() {
   // Zustand für die Module
   const [modules, setModules] = useState([]);
+  const [moduleChosen, setModuleChosen] = useState({});
 
   // URL des Backends
   const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"; 
 
   // Funktion zum Abrufen der Module
   useEffect(() => {
-    console.log("useEffect ausgeführt")
     try {
       axios
         .get(`${backendUrl}/modules/all`)
         .then((result) =>{  
-          setModules(result)//save result modules in modules
+          setModules(result.data)//save result modules in modules
         }); 
-      console.log(result);
     } catch (error) {
       console.error("Fehler beim Abrufen der Module:", error);
     }
   }, []);
- 
 
+  const chooseModule = (module) => {
+    try {
+      axios
+        .get(`${backendUrl}/modules/${module.modulnummer}`)
+        .then((result) =>{  
+          setModuleChosen(result.data);
+        });
+    } catch (error) {
+      console.error("Fehler beim Auswählen eines Moduls:", error);
+    }
+  }
 
   return (
     <Container fluid>
@@ -36,7 +45,12 @@ function ChooseModule() {
           <Dropdown.Header>Choose Module</Dropdown.Header>
           {/* Dropdown-Elemente */}
           {modules.map((module, index) => (
-            <Dropdown.Item key={index} eventKey={index}>
+            <Dropdown.Item 
+              key={index}
+              eventKey={index}
+              onClick={() => {
+                chooseModule(module);
+              }}>
               {module.titelde} / {module.titelen}
             </Dropdown.Item>
           ))}
