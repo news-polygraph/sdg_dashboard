6107b46d5ae8d7a3c0dcb60b7c3426048c9cd38e
HW0470488 <helena.werner@campus.tu-berlin.de>
2024-11-20 12:54:50 +0100

    Change get modules function to useEffect
    still doesnt work
    Error:map is not a function

--- src/components/Navbars/ChooseModule.jsx
+++ src/components/Navbars/ChooseModule.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Dropdown } from "react-bootstrap";
 import axios from "axios";
 
@@ -9,36 +9,39 @@ function ChooseModule() {
   // URL des Backends
   const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"; 
 
-  // Daten beim ersten Render abrufen
+  // Funktion zum Abrufen der Module
   useEffect(() => {
-    const fetchModules = async () => {
-      try {
-        const response = await axios.get(`${backendUrl}/modules/all?full=false`);
-        setModules(response.data); // Daten in den Zustand speichern
-      } catch (error) {
-        console.error("Fehler beim Abrufen der Module:", error);
-      }
-    };
+    console.log("useEffect ausgeführt")
+    try {
+      axios
+        .get(`${backendUrl}/modules/all`)
+        .then((result) =>{  
+          setModules(result)//save result modules in modules
+        }); 
+      console.log(result);
+    } catch (error) {
+      console.error("Fehler beim Abrufen der Module:", error);
+    }
+  }, []);
+ 
 
-    fetchModules();
-  }, []); // Leer = nur einmal beim Mounten ausführen
 
   return (
     <Container fluid>
-        <Dropdown>
-          <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-            Choose Module
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Header>Choose Module</Dropdown.Header>
-            {/* Dropdown-Elemente */}
-            {modules.map((module, index) => (
-              <Dropdown.Item eventKey={index}>
-                {module.titelde} / {module.titelen}
-              </Dropdown.Item>
-            ))}
-          </Dropdown.Menu>
-        </Dropdown>
+      <Dropdown>
+        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+          Choose Module
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Header>Choose Module</Dropdown.Header>
+          {/* Dropdown-Elemente */}
+          {modules.map((module, index) => (
+            <Dropdown.Item key={index} eventKey={index}>
+              {module.titelde} / {module.titelen}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
     </Container>
   );
 }
