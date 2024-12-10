dfa7a76e77eedbc0bb7f6ee15b5fae0bdb509f8c
HW0470488 <helena.werner@campus.tu-berlin.de>
2024-12-10 13:05:53 +0100

    Improve Feedback Section

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -4,11 +4,9 @@ import { Container, Row, Col, Card, CardHeader, CardBody,InputGroup } from "reac
 import { sdgIcons, sdgColors } from "./utils.js";
 import axios from "axios";
 import Button from 'react-bootstrap/Button';
-import { Label } from "recharts";
 import Form from 'react-bootstrap/Form';
-import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
-import { Input } from "reactstrap";
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 function FeedbackSection ({ sdgActive, sdgMissing }){
 	console.log("Props in FeedbackSection:", { sdgActive, sdgMissing });
@@ -23,37 +21,45 @@ function FeedbackSection ({ sdgActive, sdgMissing }){
 	 //saves which sdg was clicked to switch back after onMouseLeave
 	 const [sdgClicked, setSdgClicked] = useState("");
 	 
-	  // URL des Backends
-	  const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"; 
-	  //for debugging
-	  if (sdgActive === null) {
-		console.log("sdgActive is null");
-	  }
-	  
-	  // function to load sdg_descriptions
-	  useEffect(() => {
-		try {
-		  axios
-			.get(`${backendUrl}/descriptions`)
-			.then((result) =>{  
-			  setSdgDescriptions(result.data)//save result in sdgDescriptions
-			  console.log("set SDGdescriptions");
-			  console.log(result.data);
-			}); 
-		  
-		} catch (error) {
-		  console.error("cant get descriptions", error);
-		}
-	  }, []);
+	// URL des Backends
+	const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"; 
+	//for debugging
+	if (sdgActive === null) {
+	console.log("sdgActive is null");
+	}
+	
+	// function to load sdg_descriptions
+	useEffect(() => {
+	try {
+		axios
+		.get(`${backendUrl}/descriptions`)
+		.then((result) =>{  
+			setSdgDescriptions(result.data)//save result in sdgDescriptions
+			console.log("set SDGdescriptions");
+			console.log(result.data);
+		}); 
+		
+	} catch (error) {
+		console.error("cant get descriptions", error);
+	}
+	}, []);
 	 
 	  //only the missing SDGs from this section
-	  const [activeSdgDescription, setActiveSdgDescription] = useState({}); 
+	const [activeSdgDescription, setActiveSdgDescription] = useState({}); 
    
-	  //called by clicking on an sdgIcon from the missing SDGS
-	  const changeSDGActiveDescription = (number) =>{
-	   setActiveSdgDescription(sdgDescriptions.find(sdg => sdg.number==number));
-	   console.log("changed activeSdgDescription to " + sdgDescriptions.find(sdg => sdg.number==number));
-	  };
+	//called by clicking on an sdgIcon from the missing SDGS
+	const changeSDGActiveDescription = (number) =>{
+	setActiveSdgDescription(sdgDescriptions.find(sdg => sdg.number==number));
+	console.log("changed activeSdgDescription to " + sdgDescriptions.find(sdg => sdg.number==number));
+	};
+
+	//fit doesnt toggle
+	const [fitToggle, setFitToggle] = useState('0');
+
+	const fitRadios = [
+		{ name: 'fits', value: '0' },
+		{ name: 'does not fit', value: '1' },
+	];
 
 
     return (
@@ -72,19 +78,29 @@ function FeedbackSection ({ sdgActive, sdgMissing }){
 			<Row>
 				<Col md={12} lg={6}>
 					<Card>
-						<CardHeader><h5>Active SDG:</h5> </CardHeader>
+						<CardHeader><h5>Active SDG: {sdgActive}</h5> </CardHeader>
 						<CardBody>
 							<Form>
 								<Row class="row-margin-bottom">
 									<Col xs={12} sm={8}>
-										<ToggleButtonGroup type="radio" name="sdg-fits" defaultValue={1}>
-											<ToggleButton value={1} >
-												fits
+										<ButtonGroup>
+											{fitRadios.map((radio, idx) => (
+											<ToggleButton
+												key={idx}
+												id={`radio-${idx}`}
+												type="radio"
+												variant="secondary"
+												value={radio.value}
+												checked={fitToggle === radio.value}
+												onClick={() => 
+												setFitToggle(radio.value)
+												}
+												
+											>
+												{radio.name}
 											</ToggleButton>
-											<ToggleButton value={2} >
-												does not
-											</ToggleButton>
-										</ToggleButtonGroup>
+											))}
+										</ButtonGroup>
 									</Col>
 								</Row>
 								<Row class="row-margin-bottom">
@@ -95,7 +111,7 @@ function FeedbackSection ({ sdgActive, sdgMissing }){
 								</Row>
 								<Row class="row-margin-bottom">
 									<Col xs={12} sm={8}>
-										<Button class="btn-custom btn btn-fill">
+										<Button variant="secondary">
 											Send feedback for active SDG
 										</Button>
 									</Col>
@@ -110,43 +126,51 @@ function FeedbackSection ({ sdgActive, sdgMissing }){
 						<CardBody>
 							<Form>
 								<Row class="row-margin-bottom">
-								{/*Missing SDG Section */}
-								
-								{missingSDGIcons.map(({key, sdgIcon}) => (
-									<Col xs={3} md={2} xl={2} key={key} className="p-0">
-									<img
-										className="sdgIcon img-fluid"
-										src={sdgIcon}
-										key={key}
-										sdgId={key}
-										alt={"sdg_icon_".concat(key)}
-										style={{
-										objectFit: "contain",
-										paddingLeft: "0px", // remove style
-										filter:
-											key === missingSdgActive
-											? "grayscale(0%)" // if sdg is selected normal color
-											: "grayscale(70%)" // not selected less color intensity
+									<Card>
+										<CardHeader>please select a sdg which you think would also have fitted too</CardHeader>
+										<CardBody>
+											<Row>
+											{missingSDGIcons.map(({key, sdgIcon}) => (
+												<Col xs={3} md={2} xl={2} key={key} className="p-0">
+												<img
+													className="sdgIcon img-fluid"
+													src={sdgIcon}
+													key={key}
+													sdgId={key}
+													alt={"sdg_icon_".concat(key)}
+													style={{
+													objectFit: "contain",
+													paddingLeft: "0px", // remove style
+													filter:
+														key === missingSdgActive
+														? "grayscale(0%)" // if sdg is selected normal color
+														: "grayscale(90%)" // not selected less color intensity
+															
+													}}
+													onMouseEnter={() => setMissingSdgActive(key)}
+													onMouseLeave={() => setMissingSdgActive(sdgClicked)}
+													onClick={() => {
+													setMissingSdgActive(key);
+													changeSDGActiveDescription(key);
+													setSdgClicked(key);
+													}}
+												/>
 												
-										}}
-										onMouseEnter={() => setMissingSdgActive(key)}
-										onMouseLeave={() => setMissingSdgActive(sdgClicked)}
-										onClick={() => {
-										setMissingSdgActive(key);
-										changeSDGActiveDescription(key);
-										setSdgClicked(key);
-										}}
-									/>
-									
-									</Col>
-								))}
+												</Col>
+											))}
+											</Row>
+										</CardBody>
+									</Card>
 								</Row>
 								<Row class="row-margin-bottom">
 									<Card>
 										<CardHeader>
-											<Card.Title>Chosen SDG description</Card.Title>
+											<Card.Title>Chosen SDG description:</Card.Title>
 										</CardHeader>
-										<CardBody>{activeSdgDescription.description}</CardBody>
+										<CardBody>
+										{activeSdgDescription.description?activeSdgDescription.description
+										:"please select a SDG to read the description"}
+										</CardBody>
 									</Card>
 								</Row>	
 								<Row class="row-margin-bottom">
@@ -157,7 +181,7 @@ function FeedbackSection ({ sdgActive, sdgMissing }){
 								</Row>
 								<Row class="row-margin-bottom">
 									<Col xs={12} sm={8}>
-										<Button class="btn-custom btn">
+										<Button variant="secondary">
 											Send feedback for active SDG
 										</Button>
 									</Col>
