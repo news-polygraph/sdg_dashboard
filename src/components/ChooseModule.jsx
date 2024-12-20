dbf016d8d1970a8e0dc15058176a482d3be9a84c
k0nst1x <konsti.berlin@web.de>
2024-12-20 23:35:01 +0100

    Add example model request in ChooseModule.jsx

--- src/components/ChooseModule.jsx
+++ src/components/ChooseModule.jsx
@@ -44,6 +44,31 @@ function ChooseModule({setSentRequest}) {
     }
   }
 
+  const sendModelReq = (module) => {
+    // bspw sowas:
+    const m = {"modulinfos": {
+        "modulnummer": 51088,
+        "versionsnummer": 1,
+        "link": "https://moseskonto.tu-berlin.de/moses/modultransfersystem/bolognamodule/beschreibung/anzeigen.html?number=51088&version=1&sprache=1",
+        "titelde": "Medizintechnik im Krankenhaus",
+        "titelen": "Medical technology in hospitals",
+        "lernergebnissede": "Die Absolvent*innen dieses Moduls erlernen die erforderlichen Grundlagen des/der in der Medizintechnikabteilung eines Krankenhauses tätige/n Ingenieur*in bei der Anwendung dort eingesetzter Medizinprodukte. Durch die Vermittlung der zugehörigen Aufgaben und Tätigkeiten und deren relevanter Aspekte werden die Teilnehmer*innen in die Lage versetzt, Anforderungen an Medizinprodukte aus Sicht eines Krankenhauses zu verstehen. Mit Abschluss des Moduls verfügen die Absolvent*innen grundlegende Kenntnisse zur medizintechnischen Planung, zur Beschaffung sowie zum Betreiben von Medizinprodukten in einer Gesundheitseinrichtung. Die Absolvent*innen werden befähigt, Entscheidungen zur zielgerichteten Anwendung der Medizintechnik im Krankenhausumfeld zu treffen.",
+        "lernergebnisseen": "Graduates of this module learn the necessary basics of the engineer working in the medical technology department of a hospital in the application of medical devices used there. By learning about the associated tasks and activities and their relevant aspects, participants will be able to understand the requirements for medical devices from the perspective of a hospital. On completion of the module, graduates will have basic knowledge of medical technology planning, procurement and the operation of medical devices in a healthcare facility. Graduates will be able to make decisions on the targeted application of medical technology in the hospital environment.",
+        "lehrinhaltede": "• Funktionsweise und Aufbau Krankenhaus • Medizintechnische Planung • Beschaffung von Medizinprodukten • Betreiben von Medizinprodukten • Medizinische IT: Vernetzung und Informationssicherheit",
+        "lehrinhalteen": "Functionality and organisation of hospitals - Medical technology planning - Procurement of Medical devices - Operation of Medical devices - Medical IT: networking and information security"
+    }}
+    try {
+      axios
+        .post(`${backendUrl}/model`, m)
+        .then((result) =>{
+          console.log(result.data);
+        });
+        
+    } catch (error) {
+      console.error("Req Fehler", error);
+    }
+  }
+
   //language to display modules
   const [languageModuleInfo, setLanguage] = useState("deutsch");
   const changeLanguage = (language) =>{
